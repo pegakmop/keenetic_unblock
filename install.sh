c144dd202a86448a963f7f297e44e7ed1e8a538b
pegakmop <93389368+pegakmop@users.noreply.github.com>
2024-11-19 18:02:26 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,15 +9,15 @@ cd /storage || echo "Directory /storage not found"
 mkdir -p unblocker
 
 echo "Downloading files"
-curl -s https://raw.githubusercontent.com/zimarev/keenetic_unblock/master/unblock.txt > /storage/unblocker/unblock.txt 
-curl -s https://raw.githubusercontent.com/zimarev/keenetic_unblock/master/unblock.sh > /storage/unblocker/unblock.sh
+curl -s https://raw.githubusercontent.com/pegakmop/keenetic_unblock/master/unblock.txt > /storage/unblocker/unblock.txt 
+curl -s https://raw.githubusercontent.com/pegakmop/keenetic_unblock/master/unblock.sh > /storage/unblocker/unblock.sh
 chmod +x /storage/unblocker/unblock.sh
 
 echo "Creating link to cron"
-ln -s /storage/unblocker/unblock.sh /opt/etc/cron.hourly/unblock.sh
+ln -s /storage/unblocker/unblock.sh /opt/etc/cron.5mins/unblock.sh
 
 echo "Starting cron and script itself"
-/bin/sh /opt/etc/cron.hourly/unblock.sh
+/bin/sh /opt/etc/cron.5mins/unblock.sh
 /opt/etc/init.d/S10cron start
 
 echo "DONE"
